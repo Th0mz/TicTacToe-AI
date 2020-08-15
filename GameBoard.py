7fecc3d12241fc2f716bfd9beb9925309ebc1d51
Th0mz <tomastavares21122001@users.noreply.github.com>
2020-08-15 11:11:33 +0100

    Win counter

--- GameBoard.py
+++ GameBoard.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame import gfxdraw
 
+from time import sleep
+
 from Player import player1Render, player2Render
 
 _ = None
@@ -13,7 +15,7 @@ HEIGHT = 3
 class Board:
     def __init__(self, player1, player2):
         # Screen
-        self.screen_size = (600, 250)
+        self.screen_size = (700, 250)
         self.screen = pygame.display.set_mode(self.screen_size)
 
         # Board
@@ -40,6 +42,11 @@ class Board:
         # When true updates the screen
         self.update = True
 
+        # Fonts
+        self.initGUI()
+        self.winsFont = pygame.font.SysFont("Comic Sans MS", 20)
+
+
 
 
     def gameLoop(self):
@@ -57,17 +64,36 @@ class Board:
                 elif event.type == pygame.MOUSEBUTTONUP:
                     clicked = True        
 
-            if self.update:
-                self.display()
-                self.update = False
-
             # The player that is playing makes a move
             if self.player1Turn:
                 self.playerMove(self.player1, clicked, self.boardSize, self.boardPosition)
             else:
                 self.playerMove(self.player2, clicked, self.boardSize, self.boardPosition)
 
+            # Updates the screen board with the new player move
+            if self.update:
+                self.display()
+                self.update = False
+
+
+            # Cheks if someone won
+            winner = self.checkVictory()
+            if winner != None:
+                sleep(0.5)
+                if winner == self.player1.symbol:
+                    self.player1.wins += 1
+                else:
+                    self.player2.wins += 1
+
+                self.restartGame()
+
+            if self.checkTie():
+                sleep(0.5)
+                self.restartGame()
+
             clicked = False
+        
+        self.quitGUI()
 
 
     def initGUI(self):
@@ -115,15 +141,38 @@ class Board:
                     elif self.board[y][x] == self.player2.symbol:
                         position = (firstPos[X] + (x * self.boardSize // 3), firstPos[Y] + (y * self.boardSize // 3))
                         player2Render(self.screen, self.boardSize, self.boardWeight, position, player2Color, backgroundColor)
+        
+        def renderWins():
+            # Render Player symbol
+            player1Position = (self.boardPosition[X] // 2 - 50, self.screen_size[Y] // 3)
+            player1Render(self.screen, self.boardSize, self.boardWeight, player1Position, player1Color)
 
+            # Render text
+            winsText = "Wins : {}".format(self.player1.wins)
+            player1Wins = self.winsFont.render(winsText, True, (0, 0, 0))
 
+            text_width, text_height = self.winsFont.size(winsText)
+            textPosition = (player1Position[X] -text_width // 2, player1Position[Y] + 40 - text_height // 2)
+            self.screen.blit(player1Wins, textPosition)
+            
+            # Render Player symbol
+            player2Position = (self.screen_size[X] - player1Position[X], self.screen_size[Y] // 3)
+            player2Render(self.screen, self.boardSize, self.boardWeight, player2Position, player2Color, backgroundColor)
 
+            # Render text
+            winsText = "Wins : {}".format(self.player2.wins)
+            player2Wins = self.winsFont.render(winsText, True, (0, 0, 0))
+
+            text_width, text_height = self.winsFont.size(winsText)
+            textPosition = (player2Position[X] -text_width // 2, player2Position[Y] + 40 - text_height // 2)
+            self.screen.blit(player2Wins, textPosition)
         
         # Background color
         self.screen.fill(backgroundColor)
 
         renderBoard()
         renderPlayers()
+        renderWins()
 
         pygame.display.update()
 
@@ -145,10 +194,65 @@ class Board:
 
 
     def checkVictory(self):
-        return
+        """ Check if any of the players won """
+
+        def checkAllRow(board, winner):
+            """ Checks all rows """
+            if winner == None:
+                for row in board:
+                    winner = row[0]
+
+                    # Checks if the elements of the row are all the same
+                    if winner == row[1] and winner == row[2]:
+                        return winner
+            else:
+                return winner
+            
+        def checkAllColumn(board, winner):
+            """ Checks all columns """
+            if winner == None:
+                for x in range(len(board[0])):
+                    winner = board[0][x]
+
+                    # Checks if the elements of the column are all the same
+                    if winner == board[1][x] and winner == board[2][x]:
+                        return winner
+            else:
+                return winner
+
+        def checkAllDiagonal(board, winner):
+            """ Checks all diagonals"""
+            if winner == None:
+                # Check main diagonal
+                winner = board[0][0]
+                if winner == board[1][1] and winner == board[2][2]:
+                    return winner
+
+                # Check secondary diagonal
+                winner = board[0][2]
+                if winner == board[1][1] and winner == board[2][0]:
+                    return winner
+            else:
+                return winner
+        
+        # Check victory
+        winner = checkAllRow(self.board, None)
+        winner = checkAllColumn(self.board, winner)
+        winner = checkAllDiagonal(self.board, winner)
+
+        return winner
 
     def checkTie(self):
-        return
+        """ Check if it was a tie [has a blank space]"""
+        for row in self.board:
+            if _ in row:
+                return False
+        
+        return True
 
     def restartGame(self):
-        return
+        self.board = [[_, _, _],
+                      [_, _, _],
+                      [_, _, _]]
+
+        self.update = True
--- main.py
+++ main.py
@@ -2,11 +2,8 @@ from GameBoard import Board
 from Player import Player
 
 player1 = Player("X", "Player 1", False)
-player2 = Player("O", "Player 2", True)
+player2 = Player("O", "Player 2", False)
 
 board = Board(player1, player2)
 
-board.initGUI()
 board.gameLoop()
-
-board.quitGUI()
