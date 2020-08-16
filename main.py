685d411e6e8f07376d934adcce3235e495b143c5
Th0mz <tomastavares21122001@users.noreply.github.com>
2020-08-16 19:57:01 +0100

    Code design changes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from GameBoard import Board
+from Game import Game
 from Player import Player
 
 player1 = Player("X", "Player 1", False)
 player2 = Player("O", "Player 2", True)
 
-board = Board(player1, player2)
+game = Game(player1, player2)
 
-board.gameLoop()
+game.gameLoop()
